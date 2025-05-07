514d5521157af0bd05fc7f7cfabe4f5e96fe356f
Nikita Kupreev <n.kupreev@codd.tech>
2025-05-07 18:33:44 +0300

    inline css

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
 import react from '@vitejs/plugin-react';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    cssInjectedByJsPlugin(),
     dts({
       rollupTypes: true,
       tsconfigPath: './tsconfig.app.json',
